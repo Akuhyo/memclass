03373105918321ff5cec92f36a62d79b3d644370
Akuhyo <dgL7@protonmail.com>
2020-04-06 16:40:18 -0500

    Add files via upload

---
+++ memclass.cpp
@@ -0,0 +1,11 @@
+#include "include.h";
+
+void memWrite(DWORD address_to_write_to, int value)
+{
+	*(DWORD*)(address_to_write_to) = value;
+}
+
+DWORD memRead(DWORD address_to_read)
+{
+	return *(DWORD*)(address_to_read);
+}
---
+++ memclass.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "include.h";
+
+void memWrite(DWORD address_to_write_to, int value);
+
+DWORD memRead(DWORD address_to_read);
